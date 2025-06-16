21c902829e9ae4e4104a0f97d8ea573afb834038
Yoki <viktorpomidor110@gmail.com>
2025-06-16 09:21:56 +0500

    mvp

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import time
 from typing import List
+from pathlib import Path
 
 @dataclass
 class Channel:
@@ -24,8 +25,14 @@ class Channel:
 
 
 def json_to_html(json_file: str, html_file: str):
-    with open(json_file, "r", encoding="utf-8") as f:
+    with open("jsons/" + json_file, "r", encoding="utf-8") as f:
         channels = json.load(f)
+
+
+    output_dir = Path("webs")
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    full_path = output_dir / html_file
 
     html = """
 <!DOCTYPE html>
@@ -53,7 +60,7 @@ def json_to_html(json_file: str, html_file: str):
         <th onclick="sortTable(1)">Ссылка</th>
         <th onclick="sortTable(2)">Категория</th>
         <th onclick="sortTable(3)">Подписчики</th>
-        <th onclick="sortTable(4)">Средний просмотр</th>
+        <th onclick="sortTable(4)">Средний охват</th>
         <th onclick="sortTable(5)">ERR (%)</th>
         <th onclick="sortTable(6)">Миграция 7д</th>
         <th onclick="sortTable(7)">Миграция 30д</th>
@@ -102,16 +109,13 @@ def json_to_html(json_file: str, html_file: str):
 
     for (var row of rows) table.tBodies[0].appendChild(row);
 
-    // Снять старую подсветку
     var ths = table.tHead.rows[0].cells;
     for (var i = 0; i < ths.length; i++) {
       ths[i].classList.remove("sorted-asc", "sorted-desc");
     }
 
-    // Добавить новую стрелку
     ths[n].classList.add(asc ? "sorted-asc" : "sorted-desc");
 
-    // Подсветить колонку
     for (var r of rows) {
       for (var i = 0; i < r.cells.length; i++) {
         r.cells[i].classList.toggle("active-col", i == n);
@@ -127,14 +131,18 @@ def json_to_html(json_file: str, html_file: str):
 </html>
 """
 
-    with open(html_file, "w", encoding="utf-8") as f:
+    with open(full_path, "w", encoding="utf-8") as f:
         f.write(html)
 
-    print(f"✅ HTML-отчёт с визуальной сортировкой сохранён в {html_file}")
+    print(f"✅ HTML-отчёт с визуальной сортировкой сохранён в {full_path}")
 
 def save_channels_to_json(channels: List[Channel], filename: str):
-    filename += ".json"
-    with open(filename, "w", encoding="utf-8") as f:
+    output_dir = Path("jsons")
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    full_path = output_dir / (filename + ".json")
+
+    with open(full_path, "w", encoding="utf-8") as f:
         json.dump(channels, f, ensure_ascii=False, indent=2)
 
 def extract_sub7(soup : BeautifulSoup) -> int:
@@ -219,18 +227,28 @@ def extract_category(soup : BeautifulSoup) -> str | None:
 
 def parse_channel(driver, url: str) -> dict:
     driver.get(url)
-    try:
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.TAG_NAME, "h1"))
-        )
-    except:
-        print(f"[!] Не удалось дождаться загрузки: {url}")
-        return {}
 
-    soup = BeautifulSoup(driver.page_source, "html.parser")
+    while True:
+        try:
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.TAG_NAME, "h1"))
+            )
+        except:
+            print(f"[!] Не удалось дождаться загрузки h1: {url}")
+            time.sleep(15)
+            continue
 
-    return {
-        "name": extract_name(soup),
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        name = extract_name(soup)
+
+        if name:
+            break
+        else:
+            print(f"[!] Имя не найдено, возможно капча. Ждём решения капчи... ({url})")
+            time.sleep(5)
+
+    data = {
+        "name": name,
         "link": extract_link(soup),
         "category": extract_category(soup),
         "subs": extract_subs(soup),
@@ -241,68 +259,96 @@ def parse_channel(driver, url: str) -> dict:
         "lifetime": extract_lifetime(soup),
     }
 
-    
-try:
+    if not data["name"] or not data["link"]:
+        print(f"[!] Парсер не получил данные после решения капчи: {url}")
+        return {}
 
-    driver = undetected_chromedriver.Chrome()
-    # print(parse_channel(driver, "https://tgstat.ru/channel/@leoday/stat"))
-    driver.get("https://tgstat.ru/ratings/channels")
-    cookie = [
-    {"name": "_ym_uid", "value": "1702479296231132795", "path": "/"},
-    {"name": "_ym_d", "value": "1743477278", "path": "/"},
-    {"name": "tgstat_settings", "value": "f7e26f5d1be638583c1848175d4df050697604776eda4645b37467ebd1d79f6ca%3A2%3A%7Bi%3A0%3Bs%3A15%3A%22tgstat_settings%22%3Bi%3A1%3Bs%3A19%3A%22%7B%22fp%22%3A%22VGAt5zhwmM%22%7D%22%3B%7D", "path": "/"},
-    {"name": "_ym_isad", "value": "1", "path": "/"},
-    {"name": "_gid", "value": "GA1.2.1457856778.1749897055", "path": "/"},
-    {"name": "_ga", "value": "GA1.1.1897481012.1743477278", "path": "/"},
-    {"name": "cf_clearance", "value": "hjkVGXEhBlcrnT09HkAlNvOPrP04SUPMrB9IwAmQiHc-1749910808-1.2.1.1-dlFkbAvfTQkd7rowFlsuanf6uCwYxY87asE41TqilJAzxjJZtt8GFSILTH36vFkw8FMmJvQqWz4pXOcPH1Vu40OKjlv5vfDYDNf7Dl0wl4KyEjik1RMZpg2AeP5fyA4bYIDNamA1ukgAqGhZINL1Zr2RJjjmCDJnnS_sdurpHrWtUE_JbEwhCKK.MKkLN2gMwsKdAKX9nw4IgqBTxTqlHHNfsjjP514OGyyNbq9GWc_4.R8Z8LhtqWGWwa2wQpilXH8YXaHtyiUUuQM58Gsnf3RKGoJpLqhGns7iZWp11qBfKBAX6DdZSK01pYJIqGbp48FrikTK.OP7J11f9XFsngc394PNfDpHW158bt5zG5A", "path": "/"},
-    {"name": "tgstat_idrk", "value": "fc5083620d6ae75048e17cbad7199fda07185689735c3db33cc6e7e1b44c5481a%3A2%3A%7Bi%3A0%3Bs%3A11%3A%22tgstat_idrk%22%3Bi%3A1%3Bs%3A53%3A%22%5B11540621%2C%22O96_OkV-zSHY0iv-9dvMRhiko3XgZZGV%22%2C2592000%5D%22%3B%7D", "path": "/"},
-    {"name": "tgstat_sirk", "value": "imbbp0m0lbpcefs4bpedp6o6nq", "path": "/"},
-    {"name": "_tgstat_csrk", "value": "6bc090a67d2b7162abe79264daf1a05412604b15332c10f377a50609fc6e3b18a%3A2%3A%7Bi%3A0%3Bs%3A12%3A%22_tgstat_csrk%22%3Bi%3A1%3Bs%3A32%3A%225AsumaMqYkfX7z4wbrUr5TkUuojmpTv-%22%3B%7D", "path": "/"},
-    {"name": "_ga_ZEKJ7V8PH3", "value": "GS2.1.s1749910806$o14$g1$t1749910898$j35$l0$h0", "path": "/"}
-]
-    for cook in cookie:
-        driver.add_cookie(cook)
+    return data
 
-    driver.get("https://tgstat.ru/ratings/channels")
+if __name__ ==  "__main__":
+    try:
 
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.TAG_NAME, "h1"))
-    )
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
+        driver = undetected_chromedriver.Chrome()
+        driver.get("https://tgstat.ru/ratings/channels")
+        cookie = [
+            {"name": "_ym_uid", "value": "1702479296231132795", "path": "/"},
+            {"name": "_ym_d", "value": "1743477278", "path": "/"},
+            {"name": "tgstat_settings",
+             "value": "f7e26f5d1be638583c1848175d4df050697604776eda4645b37467ebd1d79f6ca%3A2%3A%7Bi%3A0%3Bs%3A15%3A%22tgstat_settings%22%3Bi%3A1%3Bs%3A19%3A%22%7B%22fp%22%3A%22VGAt5zhwmM%22%7D%22%3B%7D",
+             "path": "/"},
+            {"name": "_ym_isad", "value": "1", "path": "/"},
+            {"name": "_gid", "value": "GA1.2.1457856778.1749897055", "path": "/"},
+            {"name": "_ga", "value": "GA1.1.1897481012.1743477278", "path": "/"},
+            {"name": "cf_clearance",
+             "value": "hjkVGXEhBlcrnT09HkAlNvOPrP04SUPMrB9IwAmQiHc-1749910808-1.2.1.1-dlFkbAvfTQkd7rowFlsuanf6uCwYxY87asE41TqilJAzxjJZtt8GFSILTH36vFkw8FMmJvQqWz4pXOcPH1Vu40OKjlv5vfDYDNf7Dl0wl4KyEjik1RMZpg2AeP5fyA4bYIDNamA1ukgAqGhZINL1Zr2RJjjmCDJnnS_sdurpHrWtUE_JbEwhCKK.MKkLN2gMwsKdAKX9nw4IgqBTxTqlHHNfsjjP514OGyyNbq9GWc_4.R8Z8LhtqWGWwa2wQpilXH8YXaHtyiUUuQM58Gsnf3RKGoJpLqhGns7iZWp11qBfKBAX6DdZSK01pYJIqGbp48FrikTK.OP7J11f9XFsngc394PNfDpHW158bt5zG5A",
+             "path": "/"},
+            {"name": "tgstat_idrk",
+             "value": "fc5083620d6ae75048e17cbad7199fda07185689735c3db33cc6e7e1b44c5481a%3A2%3A%7Bi%3A0%3Bs%3A11%3A%22tgstat_idrk%22%3Bi%3A1%3Bs%3A53%3A%22%5B11540621%2C%22O96_OkV-zSHY0iv-9dvMRhiko3XgZZGV%22%2C2592000%5D%22%3B%7D",
+             "path": "/"},
+            {"name": "tgstat_sirk", "value": "imbbp0m0lbpcefs4bpedp6o6nq", "path": "/"},
+            {"name": "_tgstat_csrk",
+             "value": "6bc090a67d2b7162abe79264daf1a05412604b15332c10f377a50609fc6e3b18a%3A2%3A%7Bi%3A0%3Bs%3A12%3A%22_tgstat_csrk%22%3Bi%3A1%3Bs%3A32%3A%225AsumaMqYkfX7z4wbrUr5TkUuojmpTv-%22%3B%7D",
+             "path": "/"},
+            {"name": "_ga_ZEKJ7V8PH3", "value": "GS2.1.s1749910806$o14$g1$t1749910898$j35$l0$h0", "path": "/"}
+        ]
+        for cook in cookie:
+            driver.add_cookie(cook)
 
-    blocks = soup.select('.col.col-12.col-sm-5.col-md-5.col-lg-4')
-    all_urls = []
-    for card in blocks:
-        link = card.find('a')
-        if link and link.has_attr('href'):
-            all_urls.append(link['href'])
+        driver.get("https://tgstat.ru/ratings/channels")
 
-    categories = []
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.TAG_NAME, "h1"))
+        )
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-    container = soup.find("div", class_="list-group")
+        blocks = soup.select('.col.col-12.col-sm-5.col-md-5.col-lg-4')
+        all_urls = []
+        for card in blocks:
+            link = card.find('a')
+            if link and link.has_attr('href'):
+                all_urls.append(link['href'])
 
-    links = container.find_all("a", href=True)
+        categories = []
 
-    for link in links:
-        name = link.get_text(strip=True)
-        href = link["href"]
-        categories.append((name, href))
+        container = soup.find("div", class_="list-group")
 
-    all_channels = []
-    counter = 0
-    for ch in all_urls:
-        counter += 1
-        c = parse_channel(driver, ch)
-        all_channels.append(c)
-        print(c)
-        time.sleep(random.uniform(1, 3))
-        if counter > 10:
-            time.sleep(15)
+        links = container.find_all("a", href=True)
+
+        for link in links:
+            name = link.get_text(strip=True)
+            href = link["href"]
+            categories.append((name, href))
+        for name, url in categories:
+            json_path = Path("jsons") / (name + ".json")
+            if json_path.exists():
+                continue
+            driver.get("https://tgstat.ru" + url)
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.TAG_NAME, "h1"))
+            )
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+            blocks = soup.select('.col.col-12.col-sm-5.col-md-5.col-lg-4')
+            all_urls = []
+            for card in blocks:
+                link = card.find('a')
+                if link and link.has_attr('href'):
+                    all_urls.append(link['href'])
+            all_channels = []
             counter = 0
-    save_channels_to_json(all_channels, "TgChannels")
-    json_to_html("TgChannels.json", "channels_report.html")
+            for ch in all_urls:
+                counter += 1
+                c = parse_channel(driver, ch)
+                all_channels.append(c)
+                print(c)
+                time.sleep(random.uniform(2, 5))
+                if counter > 10:
+                    time.sleep(20)
+                    counter = 0
+            save_channels_to_json(all_channels, name)
+            json_to_html(name + ".json", name + " Отчёт.html")
+            time.sleep(60)
 
-except Exception as ex:
-    print(ex)
-finally:
-    driver.quit()
+    except Exception as ex:
+        print(ex)
+    finally:
+        driver.quit()
